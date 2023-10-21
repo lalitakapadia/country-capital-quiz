e6e88a510f2a8f225c32de191d3140c64c6d1bdc
lalitakapadia <lalita.kapadia@gmail.com>
2023-10-21 12:16:30 +0100

    added index.html css and javascript

---
+++ assets/js/data.js
@@ -0,0 +1,39 @@
+var question1 = {
+    question: "Which one of the following is not a valid javaScript data type?",
+    option1: "String",
+    option2: "Boolean",
+    option3: "Date",
+    option4: "Number",
+    answer: "Date"
+};
+localStorage.setItem("question1", JSON.stringify(question1));
+
+ var question2 = {
+    question: "Which type of javaScript language is?",
+    option1: "Obeject-Orianted",
+    option2: "Object-Based",
+    option3: "Assembly-language",
+    option4: "High-level",
+    answer: "Object-Based"
+ };
+ localStorage.setItem("question2", JSON.stringify(question2));
+
+var question3 = {
+    question: "Which one of the following also known as Conditional Expression?",
+    option1: "Alternative to if-else",
+    option2: "Switch statement",
+    option3: "If-then-else statement",
+    option4: "immediate if",
+    answer: "immediate if"
+};
+localStorage.setItem("question3", JSON.stringify(question3));
+
+var question4 = {
+    question: "In JavaScript, what is a block of statement?",
+    option1: "Conditional block",
+    option2: "block that combines a number of statements into a single compound statement",
+    option3: "both conditional block and a single statement",
+    option4: "block that contains a single statement",
+    answer: "block that combines a number of statements into a single compound statement"
+};
+localStorage.setItem("question4", JSON.stringify(question4));
---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+// ## User Story
+
+// ```
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+// ```
+
+// ## Acceptance Criteria
+
+// ```
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
+var startButton = document.querySelector(".start-button");
+//var resetButton = document.querySelector(.reset-buton);
+// var quiz = document.querySelector(".quiz");
+// var win = document.querySelector(".win");
+// var lose = document.querySelector(.lose);
+//var timeCountEl = document.querySelector(".timer-count");
+// var timer:
+// var timerCount = 60;
+// var winsCounter = 0;
+// var loseCounter = 0;
+// var isWin = false;
+// var isWin = false;
